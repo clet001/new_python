8939fe663ac9f92132c0edc8c4d73da65224733e
Clement Nwafor <nwaforclement7@gmail.com>
2026-05-13 11:17:30 +0100

    Update divisor.py

--- WeekendTask/divisor.py
+++ WeekendTask/divisor.py
@@ -1,8 +1,7 @@
 number = int(input("Enter a number: "))
 
 count = 0  
-dan=0                   
-
+                  
 for count in range(1, number):
     if number % count == 0:   
         print(count)          
